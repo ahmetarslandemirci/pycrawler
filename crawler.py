f132b9d8ae17ad3cd9952921002944fa9023eb31
Ahmet <41388184+ahmetarslandemirci@users.noreply.github.com>
2018-08-10 00:45:31 +0300

    added multithreading

--- crawler.py
+++ crawler.py
@@ -1,88 +1,141 @@
-
+# -*- coding: utf-8 -*-
 import requests
 import re
-from bs4 import BeautifulSoup
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from queue import Queue
+import threading
+import time
 
 class Crawler():
-    base_url = ""
-    http = ""
-    base = ""
-    depth = 5
-    content_regex = ""
-    keywords = [] # sayfada aranacak kelimeler
-    allowed_subdomains = []
-    except_extensions = []
-    content_class=""
-    scanned_links = []
-
-    allow_subdomain = False
-    headers = {
-            'User-Agent': 'My User Agent 1.0',
-        }
-
-    def __init__(self,base_url):
-        ## Url kontrolü yap
-        self.base_url = base_url
-        self.http = base_url.split("/")[0]
-        self.base = base_url.split("/")[2]
+    debug_mode = False
+    url = ''
+    base_url = ''
+    thread_count=4
+    queue = None
+    visited = None
+    limit = 0
+    data = None
+    data_regex = None
+    # for now  we suppose urls will be normal
+    def __init__(self,url,thread_count=4,limit = 100,data_regex=None,debug_mode=False):
+        if url[-1:]!="/":
+            url +="/"
+        Crawler.url = url
+        Crawler.thread_count = thread_count 
+        Crawler.queue = Queue()
+        Crawler.visited = set()
+        Crawler.limit = limit
+        Crawler.data = set()
+        Crawler.data_regex = data_regex
+        Crawler.debug_mode = debug_mode
     
     def parse(self):
-        if self.base_url != "":
-            links = (self.travel(self.base_url,[],5))
-            results = []
-            for link in links:
-                response = requests.get(link,headers=self.headers,verify=False)
-                parsed_html = BeautifulSoup(response.text,"lxml")
-                parsed_body = parsed_html.body
-                prettified = parsed_body.prettify()
-                res = []
-                is_found = 0
-                for keyword in self.keywords:
-                    is_found = str(prettified).find(keyword)
-                if(is_found>0):
-                    founded = re.findall(self.content_regex,prettified,re.S|re.I|re.UNICODE)
-                    if len(founded)>0:
-                        results.append(re.sub(r"\n+","\n",BeautifulSoup(founded[0],"lxml").get_text()))
-            return results
-    
-    def travel(self,link,scanned=[],depth=depth):
-        if link not in scanned and link[-4:] not in self.except_extensions:
-            scanned.append(link)
-            #print(link)
-            #print(depth)
-            if depth > 0:
-                urls = self.get_links_from_page(link)
-                for url in urls:
-                    self.travel(url,scanned,depth-1)
-        return scanned
+        if self.url[0:4]!="http":
+            print("[!] Please set a protocol for url"+self.url)
+            return []
+        if Crawler.debug_mode:
+            print("Parsing started.")
+        Crawler.visited.add(self.url)
+        for i in Crawler.normalize_links(Crawler.get_links(self.url),Crawler.url):
+            if i not in Crawler.visited:
+                if i[-4:] in [".jpg",".png",".gif",".exe",".pdf"]:
+                    continue
+                self.queue.put(i)
+                
+        #names = ["herro"]
+        names = ["herro","ceymi","merro","minik"]
+        workers = []
+        for name in names:
+            w = self.Worker(name)
+            w.setDaemon(True)
+            w.start()
+            workers.append(w)
 
-    def get_links_from_page(self,url):
-        links = []
-        try:
-            response = requests.get(url,headers=self.headers,verify=False)
-            #print(response.text)
-            regex_link = "<[ ]*a[^<>]+href=[\"']+([a-zA-Z0-9:\/\.\-%üıöğçşÜİÖĞÇŞ]+)[\"']+"
-            results = re.findall(regex_link,response.text,re.I)
-            for result in results:
-                temp = ""
-                if(len(result)>1 and result[0]=="/" and  result[1]=="/"):
-                    temp = (result.replace("//",self.http))
-                elif(result[0]=="/"):
-                    temp = (self.base_url+result[1:])
-                else:
-                    temp = (result)
-                if (temp not in links and temp != self.base_url)or temp in self.allowed_subdomains:
-                    if self.allow_subdomain== False and temp.find(self.base_url)==0:
-                        links.append(temp)
-                    elif self.allow_subdomain == True:
-                        matched = re.match('(http|https):\/\/([a-zA-Z_-]+\.)+'+self.base+'[a-zA-Z0-9\/\.\-%üıöğçşÜİÖĞÇŞ]+',temp)
-                        #print(matched)
-                        if matched != None:
-                            links.append(matched.group(0))
-
-            return (links)
-        except requests.exceptions.ConnectionError:
-            return links
+        for worker in workers:
+            worker.join()
         
+        return Crawler.data
+
+    @staticmethod
+    def get_base_url(url):
+        return (url.split("://")[1]).replace("www.","").replace("/","")
+
+    @staticmethod
+    def normalize_links(link_list,url):
+        normalized_links = []
+        for link in link_list:
+            if len(link)>1:
+                link = link.lower()
+                temp = ""
+                if link[0:2]=="//":
+                    temp = link.replace("//",url.split("://")[0]+"://")
+                elif link[0]=="/" :
+                    temp = url+link[1:]
+                elif link[0:4]!="http":
+                    temp = url + link
+                else:
+                    temp = link
+
+                if temp not in normalized_links and temp != url :
+                    matched = re.search('^(http|https):\/\/(www\.|www2\.)*'+Crawler.get_base_url(url)+'.+',temp)
+                    if matched != None:
+                        normalized_links.append(temp)
+                    
+        return normalized_links
+
+    @staticmethod
+    def get_links(url):
+        try:
+            if Crawler.debug_mode:
+                print("Looking to "+url)
+            response = requests.get(url,verify=False,timeout=5)
+            regex_link = "<[ ]*a[^<>]+href=[\"']?([^\"'\#]+)[\"']?"
+            results = re.findall(regex_link,response.text,re.I)
+
+            if Crawler.data_regex != None:
+                datas = re.findall(Crawler.data_regex,response.text,re.I)
+                for data in datas:
+                    Crawler.data.add(data)
+
+        except :
+            print("Error happened")
+            return []
+        return results
+
+    class Worker(threading.Thread):
+        worker_name = ""
+        lock = None
+        def __init__(self,name):
+            threading.Thread.__init__(self)
+            self.worker_name = name          
+            self.lock = threading.Lock()
+        
+        def run(self):
+            if Crawler.debug_mode:
+                print(self.worker_name + " started to working..")
+            while Crawler.queue.empty() == False :
+                
+                if len(Crawler.visited) > Crawler.limit:
+                    break
+                link = Crawler.queue.get()
+                if link in Crawler.visited:
+                    continue
+                if Crawler.debug_mode:
+                    print(" Visited: "+str(len(Crawler.visited)))
+                Crawler.visited.add(link)
+                
+
+                links = Crawler.normalize_links(Crawler.get_links(link),Crawler.url)
+                for i in links:
+                    if Crawler.base_url in i :
+                        if i not in Crawler.visited: 
+                            if i[-4:] in [".jpg",".png",".gif",".exe",".pdf"]:
+                                continue
+                            Crawler.queue.put(i)
+                       
+            if Crawler.debug_mode:
+                print(self.worker_name + " finished its job.")
+            #Crawler.queue.task_done()
+
+            
